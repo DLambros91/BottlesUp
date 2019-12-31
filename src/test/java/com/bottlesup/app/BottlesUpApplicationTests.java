93ce4b55b3126d3871f7deb18e6d92876c5de25d
brian <bchou515@gmail.com>
2019-12-31 16:53:13 -0500

    init project

---
+++ src/test/java/com/bottlesup/app/BottlesUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bottlesup.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BottlesUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
